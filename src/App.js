230454191c74912d084f351757723f0674fbd941
ak-empereur-7 <41908463+ak-empereur-7@users.noreply.github.com>
2021-08-25 22:26:18 +0900

    create UserCard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organizms/user/UserCard";
 import "./styles.css";
 
+const user = {
+  name: "Dan",
+  image: "https://source.unsplash.com/yihlaRCCvd4",
+  email: "aaaa@aaa.com",
+  tel: "080-1234-5678",
+  company: {
+    name: "日本株式会社"
+  },
+  website: "https://google.com"
+};
+
 export default function App() {
   return (
     <div className="App">
@@ -11,7 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
-      <UserCard />
+      <UserCard user={user} />
     </div>
   );
 }
