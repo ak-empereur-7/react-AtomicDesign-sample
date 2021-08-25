10fbf224eea060730e4b1d9468a1fb5ca08233cd
ak-empereur-7 <41908463+ak-empereur-7@users.noreply.github.com>
2021-08-25 21:48:28 +0900

    create organizms UserCard

---
+++ src/components/organizms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img src="" alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>aaaa@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>03-1234-5678</dd>
+        <dt>会社名</dt>
+        <dd>日本株式会社</dd>
+        <dt>WEB</dt>
+        <dd>http.com</dd>
+      </dl>
+    </div>
+  );
+};
