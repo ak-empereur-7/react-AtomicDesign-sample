5de9043144c59991dbe102ba91313b286889c43b
ak-empereur-7 <41908463+ak-empereur-7@users.noreply.github.com>
2021-08-28 12:12:33 +0900

    create Pages

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organizms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { Router } from "./components/router/Router";
 import "./styles.css";
 
-const user = {
-  name: "Dan",
-  image: "https://source.unsplash.com/yihlaRCCvd4",
-  email: "aaaa@aaa.com",
-  tel: "080-1234-5678",
-  company: {
-    name: "日本株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>ぼたん</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
